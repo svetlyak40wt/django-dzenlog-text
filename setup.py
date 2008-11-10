942dd79a948868eaa5c8d26eeb9a69c37abd4b86
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-11-10 23:40:19 +0300

    Scripts for setuptools.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dzenlog-text',
+    version = '0.1.0',
+    description = '''This is a simple application for textual blog, based on the '''
+                  '''django-dzenlog application.''',
+    keywords = 'django apps blogging dzenlog',
+    license = 'New BSD License',
+    author = 'Alexander Artemenko',
+    author_email = 'svetlyak.40wt@gmail.com',
+    url = 'http://github.com/svetlyak40wt/django-dzenlog-text/',
+    install_requires = ['django-dzenlog'],
+    dependency_links = ['http://aartemenko.com/media/packages.html'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages = find_packages(exclude=['example*']),
+    package_data = {
+        'templates': ['*.html'],
+    },
+    include_package_data = True,
+)
+
